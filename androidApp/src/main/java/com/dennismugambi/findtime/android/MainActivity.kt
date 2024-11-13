5610859c513e4fe0e73e9fff9962551bdb5da576
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2024-11-13 12:26:36 +0300

    creates main screen

--- androidApp/src/main/java/com/dennismugambi/findtime/android/MainActivity.kt
+++ androidApp/src/main/java/com/dennismugambi/findtime/android/MainActivity.kt
@@ -7,18 +7,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.dennismugambi.findtime.android.theme.AppTheme
+import com.dennismugambi.findtime.android.ui.MainView
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         Napier.base(DebugAntilog())
         setContent {
-            Text("Test")
+
+            MainView {
+
+                TopAppBar(
+                    title = {
+                        when(it) {
+                            0 -> Text(text = stringResource(R.string.world_clocks))
+                            else -> Text(text = stringResource(R.string.findmeeting))
+                        }
+                    })
+            }
         }
     }
 }
